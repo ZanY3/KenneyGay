59df31babf0b08ffaa99107cd23dcbb9b7f2db61
ZanY3 <cebureksteam@gmail.com>
2025-07-19 10:13:25 +0300

    Revert "5"
    This reverts commit a7583bf6d1fbfe61b384fdb4a681965a00c2c37b.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField, Tooltip("Скорость передвижения")] private float speed = 5f;
-    [SerializeField, Tooltip("Ускорение при старте движения")] private float acceleration = 20f;
-    [SerializeField, Tooltip("Трение при остановке")] private float deceleration = 20f;
-    [SerializeField, Tooltip("Множитель для спринта")] private float sprintMultiplier = 1.5f;
-    [SerializeField, Tooltip("Ключ спринта")] private KeyCode sprintKey = KeyCode.LeftShift;
+    public float Speed = 5f;
 
     private Rigidbody2D rb;
+    private float moveHorizontal, moveVertical;
     private Animator animator;
-    private Vector2 currentVelocity;
 
-    private static readonly int IsRunningHash = Animator.StringToHash("isRunning");
-    private static readonly int MoveXHash = Animator.StringToHash("moveX");
-    private static readonly int MoveYHash = Animator.StringToHash("moveY");
-
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -28,32 +19,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        // Читаем ввод
-        Vector2 input = new Vector2(
-            Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical")
-        ).normalized;
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
 
-        // Определяем целевую скорость
-        float targetSpeed = speed * (Input.GetKey(sprintKey) ? sprintMultiplier : 1f);
-        Vector2 targetVelocity = input * targetSpeed;
+        if (rb.velocity.magnitude > 0.1f)
+            animator.SetBool("isRunning", true);
 
-        // Интерполируем скорость для плавности
-        float accel = (input.magnitude > 0.1f) ? acceleration : deceleration;
-        currentVelocity = Vector2.MoveTowards(currentVelocity, targetVelocity, accel * Time.deltaTime);
+        else
+            animator.SetBool("isRunning", false);
 
-        // Анимация
-        bool isRunning = currentVelocity.sqrMagnitude > 0.01f;
-        animator.SetBool(IsRunningHash, isRunning);
-        if (isRunning)
-        {
-            animator.SetFloat(MoveXHash, currentVelocity.x);
-            animator.SetFloat(MoveYHash, currentVelocity.y);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = currentVelocity;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
+        rb.velocity = movement * Speed;
     }
 }
